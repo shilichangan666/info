26a20a02ac7851f07a601f3e2a0b95cea6e5d414
duanwenjie@lp.cn <3221054579@qq.com>
2024-03-12 22:37:27 +0800

    定义视图函数

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+"""
+1.  我们的模型类需要继承自model.Model
+2.  系统会自动为我们添加一个主键--id
+3.  字段
+    字段名=model.类型（选项）
+    
+    字段名就是数据表的字段名
+    
+    char(M)
+    varchar(M)
+    M就是选项
+"""
+class Book(models.Model):
+    #id
+    name = models.CharField(max_length=10)
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    # 外键约束：人物属于哪本书
+    book = models.ForeignKey(Book,on_delete=models.CASCADE)
