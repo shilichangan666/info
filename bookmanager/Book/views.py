46c99aa62efe2194b358c2128998c0bda65c09a0
duanwenjie@lp.cn <3221054579@qq.com>
2024-03-21 11:31:56 +0800

    分页操作

--- bookmanager/Book/views.py
+++ bookmanager/Book/views.py
@@ -14,7 +14,7 @@ def index(request):
 
 
 # 增加方式一：
-from Book.models import Book
+from Book.models import Book,PeopleInfo
 
 # Book = Book(
 #     name='Django',
@@ -59,55 +59,91 @@ from Book.models import Book
 
 #####################################过滤查询##########################################
 # 查询编号为1的图书
-book = Book.objects.filter(id__exact=1)
-print(book)
+# book = Book.objects.filter(id__exact=1)
+# print(book)
 # 查询书名包含'湖'的图书
-book = Book.objects.filter(name__contains='传')
-print(book)
+# book = Book.objects.filter(name__contains='传')
+# print(book)
 # 查询书名以'部'结尾的图书
-book = Book.objects.filter(name__endswith='部')
-print(book)
+# book = Book.objects.filter(name__endswith='部')
+# print(book)
 # 查询书名为空的图书
-book=Book.objects.filter(name__isnull=True)
-print(book)
+# book=Book.objects.filter(name__isnull=True)
+# print(book)
 # 查询编号为1或3或5的图书
-book = Book.objects.filter(id__in=[1,3,5])
-print(book)
+# book = Book.objects.filter(id__in=[1,3,5])
+# print(book)
 # 查询编号大于3的图书
-book = Book.objects.filter(id__gt=3)
-print(book)
+# book = Book.objects.filter(id__gt=3)
+# print(book)
 # 查询1980年发表的图书
-book = Book.objects.filter(pub_data__year=1980)
-print(book)
+# book = Book.objects.filter(pub_data__year=1980)
+# print(book)
 # 查询1990年1月1日后发表的图书
-book=Book.objects.filter(pub_data__gt='1990-1-1')
-print(book)
+# book=Book.objects.filter(pub_data__gt='1990-1-1')
+# print(book)
 
 # F对象
 # 查询阅读量大于等于评论量的图书。
-from django.db.models import F, Q
-book = Book.objects.filter(readcount__gt=F('commentcount'))
-print(book)
+# from django.db.models import F, Q
+# book = Book.objects.filter(readcount__gt=F('commentcount'))
+# print(book)
 # 查询阅读量大于2倍评论量的图书。
-book = Book.objects.filter(readcount__gt=F('commentcount')*2)
-print(book)
+# book = Book.objects.filter(readcount__gt=F('commentcount')*2)
+# print(book)
 # 查询阅读量大于20，并且编号小于3的图书
-book = Book.objects.filter(readcount__gt=20,id__lt=3)
-print(book)
+# book = Book.objects.filter(readcount__gt=20,id__lt=3)
+# print(book)
 # 查询阅读量大于20，或编号小于3的图书
-book = Book.objects.filter(Q(readcount__gt=20) | Q(id__lt=3))
-print(book)
+# book = Book.objects.filter(Q(readcount__gt=20) | Q(id__lt=3))
+# print(book)
 # 查询编号不等于3的图书
-book = Book.objects.filter(~Q(id=3))
-print(book)
+# book = Book.objects.filter(~Q(id=3))
+# print(book)
 # 查询图书的总阅读量。
-from django.db.models import Sum
-book = Book.objects.aggregate(Sum('readcount'))
-print(book)
+# from django.db.models import Sum
+# book = Book.objects.aggregate(Sum('readcount'))
+# print(book)
 # 查询图书总数。
-book = Book.objects.count()
-print(book)
+# book = Book.objects.count()
+# print(book)
 # 根据阅读量进行排序
-book = Book.objects.all().order_by('readcount')
+# book = Book.objects.all().order_by('readcount')
+# print(book)
+
+# 级联查询
+    #关联查询
+# 查询书籍为1的所有人物信息
+book = Book.objects.get(id=1)
+person = book.peopleinfo_set.all()
+print(person)
+# 查询人物为1的书籍信息
+people = PeopleInfo.objects.get(id=1)
+book = people.book
 print(book)
 
+# 关联过滤查询
+# 查询图书，要求图书人物为"郭靖"
+book = Book.objects.filter(peopleinfo__name='郭靖')
+print(book)
+# 查询图书，要求图书中人物的描述包含"八"
+book = Book.objects.filter(peopleinfo__discription__contains='八')
+print(book)
+# 查询书名为“天龙八部”的所有人物
+person = PeopleInfo.objects.filter(book__name='天龙八部')
+print(person)
+# 查询图书阅读量大于30的所有人物
+person = PeopleInfo.objects.filter(book__readcount__gt=30)
+print(person)
+
+# 分页
+# 查询结果集
+book = Book.objects.all()
+# 导入分页类
+from django.core.paginator import Paginator
+# 创建分页实例
+paginator = Paginator(book,2)
+# 获取制定页码的数据
+page_book = paginator.page(1)
+# 获取分页数据
+total = paginator.num_pages
